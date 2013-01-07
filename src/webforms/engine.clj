873c21b34e59cef613cb62e79580d3b53afb8c7f
Jim Barritt <jim@planet-ix.com>
2013-01-07 11:54:17 +0000

    [jmdb] Start of conversion to html

--- src/webforms/engine.clj
+++ src/webforms/engine.clj
@@ -7,24 +7,44 @@
   (format "Hello %s" foo))
 
 (defn path-to [field]
-  (let [{:keys [section id]} field]
-    (format "%s%s" section id)))
+  (let [{:keys [section name]} field]
+    (format "%s%s" section name)))
 
 (def about-you-form 
-  {:is :form-definition
-   :fields {:first-name {:id :first-name
-                         :section :contact-details
-                         :html q:text}}
+  {:is [:form :meta]
+   :name :about-you
+   :tags [:contact]
+   :fields [ {:is [:field :meta]
+              :name :first-name
+              :section :contact-details
+              :html q:text
+              :options [:max-length 20]
+              :validators [[range 0 10]]}
+             {:is [:field :meta]
+              :name :second-name
+              :section :contact-details
+              :html q:text}]
    :validators [check-name-and-age]})
+
+(defn html-field [field]
+  (path-to field))
+
+;; TODO - we should be able to use multimethods here, keyed on the type from :is
+(defn html-for [form]
+  (let [{:keys [name fields]} form]
+    (map html-field (-> about-you-form :fields))))
+
+(html-for about-you-form)
+
+
 
 ;;(path-to {:id "foo" :section "ho"})
 ;;(path-to (-> about-you-form :fields :first-name))
 ;;((-> about-you-form :fields (get 0) :html) "jim")
 
-(let [{:keys [a]} {:a "foo"}]
-  a)
+;;(println (map path-to (-> about-you-form :fields)))
 
-(:is about-you-form)
-(is? about-you-form)
+
+
 
 
